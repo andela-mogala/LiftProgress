7feba5862afe70bcfa44c1fe6fd69650c8800b06
Michael Ogala <michael.ogala@andela.com>
2017-04-29 01:05:35 +0100

    render react view and add ability to create a lift

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,2 +1,28 @@
 class LiftsController < ApplicationController
+  def index
+    @lifts = Lift.all
+  end
+
+  def create
+    @lift = Lift.new(lift_params)
+    if @lift.save
+      render json: @lift
+    else
+      render json: @lift.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def lift_params
+    params
+      .require(:lift)
+      .permit(
+        :date,
+        :lift_name,
+        :is_metric,
+        :weight_lifted,
+        :reps_performed,
+        :one_rm
+      )
+    end
 end
